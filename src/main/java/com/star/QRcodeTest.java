a8ab10302b96770b5a687a6e796f8cf111a4241a
shixingxing <shixingxing@yungengxin.com>
2021-09-17 12:58:22 +0800

    zxing src

---
+++ src/main/java/com/star/QRcodeTest.java
@@ -0,0 +1,31 @@
+package com.star;
+
+import javax.management.RuntimeErrorException;
+
+public class QRcodeTest {
+
+    static String image_parm = "";
+    static String save_path = "";
+    static String url_parm = "";
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 0) {
+            for (String arg : args) {
+                if (arg.startsWith("url=")) {
+                    url_parm = arg.trim().toLowerCase().split("=")[1];
+                } else if (arg.startsWith("image=")) {
+                    image_parm = arg.trim().toLowerCase().split("=")[1];
+                } else if (arg.startsWith("save=")) {
+                    save_path = arg.trim().toLowerCase().split("=")[1];
+                }
+            }
+            if (url_parm == "") {
+                url_parm = "http://www.so.com";
+            }
+            QRCodeUtil.encode(url_parm, image_parm, save_path, true);
+            return;
+        }
+        throw new RuntimeErrorException((Error) null, "url or image save path are mandatory!");
+    }
+
+}
